76673ad2d86ad2d543c3140843b88c87cc39da4b
Gabriel <ghbranco6@gmail.com>
2025-01-30 21:34:03 -0300

    fix: .env variables

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
-import fs from "fs";
-import dotenv from "dotenv";
-
-const env = dotenv.parse(fs.readFileSync(".env"));
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  env: env,
   images: {
     remotePatterns: [
       {
